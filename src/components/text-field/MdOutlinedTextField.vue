80109d8013f781a62eff42ff71eda192dd9d55d4
mylmz10 <muammer.yilmaz.x@gmail.com>
2022-07-03 00:47:07 +0300

    fix: value set of OutlinedTextField

--- src/components/text-field/MdOutlinedTextField.vue
+++ src/components/text-field/MdOutlinedTextField.vue
@@ -1,7 +1,7 @@
 <template>
   <span class="md-outlined-text-field">
-    <MdOutlinedField :label="label">
-      <MdTextFieldBase />
+    <MdOutlinedField :label="label" :populated="!!inputValue">
+      <MdTextFieldBase :value="inputValue" @input="inputValue = $event.target.value" />
       <template #supporting-text>
         <slot name="supporting-text" />
       </template>
@@ -10,17 +10,24 @@
 </template>
 
 <script setup>
-import { defineProps } from 'vue';
+import { defineProps, ref, onBeforeMount } from 'vue';
 import MdTextFieldBase from './MdTextFieldBase.vue';
 import MdOutlinedField from '../field/MdOutlinedField.vue';
 
-defineProps({
+const props = defineProps({
   disabled: { type: Boolean },
   error: { type: Boolean },
   label: { type: String },
   populated: { type: Boolean },
   required: { type: Boolean },
   focused: { type: Boolean },
+  value: { type: String },
+});
+
+const inputValue = ref('');
+
+onBeforeMount(() => {
+  inputValue.value = props.value;
 });
 </script>
 
@@ -33,5 +40,15 @@ $theme: tokens.md-comp-outlined-text-field-values();
 .md-outlined-text-field {
   height: map.get($theme, container-height);
   border-radius: map.get($theme, container-shape);
+
+  .md-text-field__input {
+    caret-color: map.get($theme, caret-color);
+    color: map.get($theme, input-text-color);
+    font-family: map.get($theme, input-text-font);
+    line-height: map.get($theme, input-text-line-height);
+    font-size: map.get($theme, input-text-size);
+    font-weight: map.get($theme, input-text-weight);
+    letter-spacing: map.get($theme, input-text-tracking);
+  }
 }
 </style>
